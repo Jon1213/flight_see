1b5a584d36df5e1c71dbee5b790e6d10d618c377
Jon1213 <jon1213@gmail.com>
2014-10-28 11:09:33 -0700

    a nice message

--- app.js
+++ app.js
@@ -158,7 +158,7 @@ app.get('*', function(req,res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("get this party started on port 3000");
 });
 
